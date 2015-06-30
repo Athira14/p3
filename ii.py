176b0385b89a2ceca3eb41d44243af60c60f6cda
Athira14 <athirabalachandran333@gmail.com>
2015-06-30 09:49:03 +0530

    initial commit

---
+++ ii.py
@@ -0,0 +1,11 @@
+﻿string=input("enter the string")
+count={}
+for char in string:
+ if char in count.keys():
+    count[char] +=1
+ else:
+    count[char] =1
+for i in count:   
+  
+ print ('%s' %(i))
+ print ('*\n'*count[i],)
